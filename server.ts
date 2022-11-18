d240e639be95772a2bf157c2640782669c06232e
JohnJM <drndrndrndrone@gmail.com>
2022-11-18 14:40:01 +0000

    rm

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import express, { Express } from "express";
-import cors from 'cors';
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import rateLimit from "express-rate-limit";
 import morgan from "morgan";
@@ -12,19 +12,20 @@ const rateLimiter = rateLimit(RATE_LIMIT_OPTIONS);
 
 const createServer = async () => {
     await prisma.$connect();
-    // await prisma.category.deleteMany({})
     return express();
 };
 const main = (app: Express) => {
-    const { SERVER_ONLINE_MESSAGE, SERVER_PORT, STATIC_DIRECTORY } =
-        CONSTANTS;
+    const { SERVER_ONLINE_MESSAGE, SERVER_PORT, STATIC_DIRECTORY } = CONSTANTS;
     app.listen(SERVER_PORT, () => console.log(SERVER_ONLINE_MESSAGE));
     app.use(rateLimiter);
     app.use(morgan("dev"));
     app.use(cors());
     app.use(express.json());
-    app.use(`/${STATIC_DIRECTORY}`, express.static(path.join(__dirname, STATIC_DIRECTORY)));
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(
+        `/${STATIC_DIRECTORY}`,
+        express.static(path.join(__dirname, STATIC_DIRECTORY))
+    );
+    app.use(express.static(path.join(__dirname, "/client/build")));
     app.use(routes);
 };
 
