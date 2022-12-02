c8edb07ed8a7a7d3729c7d146f0671a10b7a0af4
Jamie Gunn <jamie.gunn@and.digital>
2022-12-02 15:11:21 +0000

    remove CORS

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import path from "path";
 import express, { Express } from "express";
-import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import rateLimit from "express-rate-limit";
 import morgan from "morgan";
@@ -14,12 +13,12 @@ const createServer = async () => {
   await prisma.$connect();
   return express();
 };
+
 const main = (app: Express) => {
   const { SERVER_ONLINE_MESSAGE, SERVER_PORT, STATIC_DIRECTORY } = CONSTANTS;
   app.listen(SERVER_PORT, () => console.log(SERVER_ONLINE_MESSAGE));
   app.use(rateLimiter);
   app.use(morgan("dev"));
-  app.use(cors());
   app.use(express.json());
   app.use(
     `/${STATIC_DIRECTORY}`,
